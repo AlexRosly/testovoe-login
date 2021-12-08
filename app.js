36242adc2a7efd469307c7339f293689d1317daa
AlexRosly <rosly.a777@gmail.com>
2021-12-08 20:41:44 +0200

    marafon-4-1

--- app.js
+++ app.js
@@ -14,25 +14,42 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
 
-  square.addEventListener('mouseover', () => setColor(square));
+  // square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseover', setColor);
 
-  square.addEventListener('mouseleave', () => removeColor(square));
+  // square.addEventListener('mouseleave', () => removeColor(square));
+  square.addEventListener('mouseleave', removeColor);
 
   board.append(square);
 }
 
-function setColor(elemetn) {
+// function setColor(elemetn) {
+//   const color = getRandomColor();
+//   elemetn.style.backgroundColor = color;
+//   elemetn.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+// }
+function setColor(e) {
+  const element = e.target;
   const color = getRandomColor();
-  elemetn.style.backgroundColor = color;
-  elemetn.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(elemetn) {
-  elemetn.style.backgroundColor = '#1d1d1d';
-  elemetn.style.boxShadow = '0 0 2px #000';
+// function removeColor(elemetn) {
+//   elemetn.style.backgroundColor = '#1d1d1d';
+//   elemetn.style.boxShadow = '0 0 2px #000';
+// }
+function removeColor(e) {
+  const element = e.target;
+
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
 }
 
+// function getRandomColor() {
+//   const idx = Math.floor(Math.random() * colors.length);
+//   return colors[idx];
+// }
 function getRandomColor() {
-  const idx = Math.floor(Math.random() * colors.length);
-  return colors[idx];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
